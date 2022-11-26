e76d67f8dae7bc32b3f626d95f95891fb3409a66
KashbullinRail <kashbullinrail@gmail.com>
2022-11-26 11:45:58 +0300

    Initial commit реализована передача данных через bundle добавлены библиотеки для реализации через safeargs

---
+++ app/src/main/java/com/example/bundle_safeargs_nav_graf/FragmentOne.kt
@@ -0,0 +1,40 @@
+package com.example.bundle_safeargs_nav_graf
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.navigation.fragment.findNavController
+
+class FragmentOne : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_one, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btnGo:Button = view.findViewById(R.id.btnGo)
+        val etText: EditText = view.findViewById(R.id.etText)
+
+        val bundle = Bundle()
+        btnGo.setOnClickListener {
+            val name = etText.text
+            val hello = "Привет, $name"
+
+            bundle.putString("MyArgsNav", hello)
+            findNavController().navigate(R.id.fragmentTwo, bundle)
+        }
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/bundle_safeargs_nav_graf/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.bundle_safeargs_nav_graf
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
